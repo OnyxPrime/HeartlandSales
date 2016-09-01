90dc506eda392bcf5cf0856e657394c2051a3a5c
Ryan Overton <ryan.overton@outlook.com>
2016-09-01 00:29:29 -0500

    Server routing take 9

--- build/server.js
+++ build/server.js
@@ -9,8 +9,21 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(staticRoot));
 
-app.get('/', function(req, res) {
-    res.sendfile(__dirname+'/index1.html')
+app.use(function(req, res, next){
+
+    // if the request is not html then move along
+    var accept = req.accepts('html', 'json', 'xml');
+    if(accept !== 'html'){
+        return next();
+    }
+
+    // if the request has a '.' assume that it's for a file, move along
+    var ext = path.extname(req.path);
+    if (ext !== ''){
+        return next();
+    }
+
+    fs.createReadStream(staticRoot + 'index.html').pipe(res);
 
 });
 
