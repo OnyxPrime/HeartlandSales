74bb1d3ca18a8a3d5d4f7d19f96d33bcc23e331b
Ryan Overton <ryan.overton@outlook.com>
2016-08-31 22:25:01 -0500

    server routing fix take 4

--- build/index.js
+++ build/index.js
@@ -11,17 +11,7 @@ app.use(express.static(staticRoot));
 
 app.use(function(req, res, next){
 
-    // if the request is not html then move along
-    var accept = req.accepts('html', 'json', 'xml');
-    if(accept !== 'html'){
-        return next();
-    }
-
-    // if the request has a '.' assume that it's for a file, move along
-    var ext = path.extname(req.path);
-    if (ext !== ''){
-        return next();
-    }
+    
 
     fs.createReadStream(staticRoot + 'index.html').pipe(res);
 
