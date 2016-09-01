e26da55bfdc644e4780c67fc31f0990b5045b758
Ryan Overton <ryan.overton@outlook.com>
2016-08-31 23:09:14 -0500

    Server routing Take 5

--- build/index.js
+++ build/index.js
@@ -10,17 +10,22 @@ app.set('port', (process.env.PORT || 3000));
 app.use(express.static(staticRoot));
 
 app.use(function(req, res, next){
+    console.log('request', req);
+    console.log('response', res);
+    // if the request is not html then move along
+    var accept = req.accepts('html', 'json', 'xml');
+    if(accept !== 'html'){
+        return next();
+    }
 
-    
+    // if the request has a '.' assume that it's for a file, move along
+    var ext = path.extname(req.path);
+    if (ext !== ''){
+        return next();
+    }
 
     fs.createReadStream(staticRoot + 'index.html').pipe(res);
 
-});
-
-app.all('*', function (req, res) { res.status(200).sendFile(path.join(__dirname, '/index.html')); });
-
-app.get('*', function(req, res, next) {
-  res.sendfile("index.html");
 });
 
 app.listen(app.get('port'), function() {
