64f3e98270c1f57d7867853ee088ecc79f560d72
Ryan Overton <ryan.overton@outlook.com>
2016-10-27 17:41:31 -0700

    Add Manufacturer Bugs

--- app/services/manufacturer.service.ts
+++ app/services/manufacturer.service.ts
@@ -14,7 +14,7 @@ export class ManufacturerService{
                 url: 'http://www.americanstandard-us.com/' 
             },{
                 id: 2, 
-                cssClassName: '', 
+                cssClassName: 'blackswan', 
                 name:'Black Swan', 
                 statesRepresented:'OK | KS | Western MO | AR', 
                 blurb:'Black Swan Mfg. Co. is one of the world’s leading manufacturers and distributors of plumbing chemicals and accessories. Our major manufactured products include plumbers putty, pipe joint compounds, hand cleaners, wax gaskets for setting toilet bowls, pipe lubricants, cutting oils, solvent cements and primers for plastic pipe, drain pipe openers, and much more. Our major products that we distribute are PTFE Tape, sand cloth, open mesh cloth, metal cement cans, brushes, closet bolts, water meter keys and no-hub couplings.', 
@@ -32,7 +32,7 @@ export class ManufacturerService{
 //            {id: 4, cssClassName: 'safetyTubs', name:'Safety Tubs', statesRepresented:'OK', blurb:'Safety Tubs® designs and manufactures award winning acrylic and gelcoat walk in bathtubs. Our revolutionary walk in tubs are the premium bathtub for a safer, healthier and more comfortable bathing experience.', image: 'SafetyTubsLogo.png' },
             {
                 id: 5, 
-                cssClassName: '', 
+                cssClassName: 'vpcGlobal', 
                 name:'VPC Global', 
                 statesRepresented:'KS | MO', 
                 blurb:'Quality products that meet the industrial, commercial and residential demands of the construction industry with engineering innovation', 
@@ -40,7 +40,7 @@ export class ManufacturerService{
                 url: 'http://vpcglobal.com/' 
             },{
                 id: 6, 
-                cssClassName: '', 
+                cssClassName: 'superGreen', 
                 name:'Super Green', 
                 statesRepresented:'OK | KS | MO | AR', 
                 blurb:'SuperGreen Inc. was founded in 2008 by Henry Mai, who established many small businesses with a great mind for innovation and ingenuity. After a few bad experiences with residential hot water heater designs, Henry Mai quickly understood the need to find a better solution for consumers and aimed to resolve the challenges these products face today. His solution was a revolutionary infrared tankless water heater.', 
@@ -48,7 +48,7 @@ export class ManufacturerService{
                 url: 'https://www.supergreenusa.com/' 
             },{
                 id: 7, 
-                cssClassName: '', 
+                cssClassName: 'yanex', 
                 name:'Yanex', 
                 statesRepresented:'OK | KS | MO | AR', 
                 blurb:'One of the largest secrets of the heating industry is the aluminum bi-metal radiators. This compound of aluminum and steel has the low thermal inertia needed to transfer heat efficiently, exceeding that of copper, bronze, and steel.', 
--- build/app/services/manufacturer.service.js
+++ build/app/services/manufacturer.service.js
@@ -21,7 +21,7 @@ var ManufacturerService = (function () {
                 url: 'http://www.americanstandard-us.com/'
             }, {
                 id: 2,
-                cssClassName: '',
+                cssClassName: 'blackswan',
                 name: 'Black Swan',
                 statesRepresented: 'OK | KS | Western MO | AR',
                 blurb: 'Black Swan Mfg. Co. is one of the world’s leading manufacturers and distributors of plumbing chemicals and accessories. Our major manufactured products include plumbers putty, pipe joint compounds, hand cleaners, wax gaskets for setting toilet bowls, pipe lubricants, cutting oils, solvent cements and primers for plastic pipe, drain pipe openers, and much more. Our major products that we distribute are PTFE Tape, sand cloth, open mesh cloth, metal cement cans, brushes, closet bolts, water meter keys and no-hub couplings.',
@@ -34,12 +34,12 @@ var ManufacturerService = (function () {
                 statesRepresented: 'OK | KS | MO | AR',
                 blurb: 'Goss offers a wide variety of torches and accessories to fit any contractors need.  Our high quality Oxy-Fuel, Air-Acetylene, Air-Propane, Air-Propylene ( MAP-Pro™) and Nitrogen equipment is individually tested and engineered for performance.  We also provide high quality replacement cutting, welding, and heating tips for popular torches.',
                 image: 'GossLogo.png',
-                url: ''
+                url: 'http://www.gossonline.com/'
             },
             //            {id: 4, cssClassName: 'safetyTubs', name:'Safety Tubs', statesRepresented:'OK', blurb:'Safety Tubs® designs and manufactures award winning acrylic and gelcoat walk in bathtubs. Our revolutionary walk in tubs are the premium bathtub for a safer, healthier and more comfortable bathing experience.', image: 'SafetyTubsLogo.png' },
             {
                 id: 5,
-                cssClassName: '',
+                cssClassName: 'vpcGlobal',
                 name: 'VPC Global',
                 statesRepresented: 'KS | MO',
                 blurb: 'Quality products that meet the industrial, commercial and residential demands of the construction industry with engineering innovation',
@@ -47,7 +47,7 @@ var ManufacturerService = (function () {
                 url: 'http://vpcglobal.com/'
             }, {
                 id: 6,
-                cssClassName: '',
+                cssClassName: 'superGreen',
                 name: 'Super Green',
                 statesRepresented: 'OK | KS | MO | AR',
                 blurb: 'SuperGreen Inc. was founded in 2008 by Henry Mai, who established many small businesses with a great mind for innovation and ingenuity. After a few bad experiences with residential hot water heater designs, Henry Mai quickly understood the need to find a better solution for consumers and aimed to resolve the challenges these products face today. His solution was a revolutionary infrared tankless water heater.',
@@ -55,7 +55,7 @@ var ManufacturerService = (function () {
                 url: 'https://www.supergreenusa.com/'
             }, {
                 id: 7,
-                cssClassName: '',
+                cssClassName: 'yanex',
                 name: 'Yanex',
                 statesRepresented: 'OK | KS | MO | AR',
                 blurb: 'One of the largest secrets of the heating industry is the aluminum bi-metal radiators. This compound of aluminum and steel has the low thermal inertia needed to transfer heat efficiently, exceeding that of copper, bronze, and steel.',
